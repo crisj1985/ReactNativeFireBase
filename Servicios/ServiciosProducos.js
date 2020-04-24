aec10d970e10feaf60ec7c98f585c8daf06153de
crisj1985 <crisj1985@hotmail.com>
2020-04-23 22:03:26 -0500

    CRUD: mostrar(flatlist) desde firebase

--- Servicios/ServiciosProducos.js
+++ Servicios/ServiciosProducos.js
@@ -12,11 +12,26 @@ const crearProducto = (producto, onSuccess, onError) => {
         })
 }
 
-const registrarListener = () => {
+const registrarListener = (fnPintar) => {
+    productos = [];
     global.firestoreBD
         .collection('productos')
-        .onSnapshot(() => {
-            Alert.alert('cambio sobre productos');
+        .onSnapshot((snapShotCambios) => {
+            let cambios = snapShotCambios.docChanges();
+            let itemCambio;
+            for (let i = 0; i < cambios.length; i++) {
+                itemCambio = cambios[i];
+                if ((itemCambio.type == "added")) {
+                    productos.push(itemCambio.doc.data())
+                    Alert.alert("Cambio Added");
+                } else if ((itemCambio.type == "removed")) {
+                    Alert.alert("Cambio removed");
+                } else if ((itemCambio.type == "modified")) {
+                    Alert.alert("Cambio modified");
+                }
+
+            }
+            fnPintar(productos);
         })
 }
 
--- screens/ListaProductos.js
+++ screens/ListaProductos.js
@@ -2,18 +2,42 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import ActionButton from "react-native-action-button";
 import Icon from "react-native-vector-icons/Ionicons";
+import { FlatList } from "react-native-gesture-handler";
+import { ItemProducto } from "./itemProductos";
+import {registrarListener} from '../Servicios/ServiciosProducos'
 
 export class ListaProductos extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    listaProductos : []
+  }
+  
+}
+
+pintar= (productos)=>{
+  this.setState({
+    listaProductos: productos,
+  });
+
+}
+
+componentDidMount(){
+registrarListener(this.pintar);
+}
+
   render() {
     return (
       <View style={styles.container}>
         <Text>LISTA DE PRODUCTOS</Text>
-        <Button
-          title="Formulario Productos"
-          onPress={() => {
-            this.props.navigation.navigate("StackFormularioProducto");
-          }}
-        ></Button>
+       <FlatList
+       data={this.state.listaProductos}
+       renderItem = {({item})=>{
+       return <ItemProducto productos={item}/>
+       }}
+       keyExtractor = {item=>parseInt( item.id)}
+       />
         <ActionButton buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item
             buttonColor="#9b59b6"
