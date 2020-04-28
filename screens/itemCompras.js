2fc281037f15ef86bce8531fec3e965ee65b30f1
crisj1985 <crisj1985@hotmail.com>
2020-04-27 19:59:05 -0500

    estilos

--- screens/itemCompras.js
+++ screens/itemCompras.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Text, View, Button, Alert,TouchableHighlight } from 'react-native'
+import { Text, View, Button, Alert, TouchableHighlight, StyleSheet } from 'react-native'
 import { eliminarElementoFB} from '../Servicios/ServiciosProducos'
 import { NavigationContext } from '@react-navigation/native';
+import { Avatar } from 'react-native-elements';
 
 export class ItemCompra extends Component {
 
@@ -14,28 +15,71 @@ export class ItemCompra extends Component {
   };
 
     render() {
-        const {productos} = this.props 
+        const {productos,nav} = this.props 
         const {id,nombre,precio} = productos
         return (
-          <View>
-            {/* <TouchableHighlight
-            onPress={()=>{
-                nav.navigate("StackFormularioProducto",{producto:productos});
-            }}
-            > */}
-              <View><Text> {id} {nombre} </Text>
-                <Text> {precio} </Text>
-                </View>
-              
-            {/* </TouchableHighlight> */}
-            
-            {/* <Button
-            title='Eliminar'
-            onPress={()=>{
-              eliminarElementoFB(id, this.OnSuccess, this.OnError);
-            }}
-            ></Button> */}
-          </View>
+          <TouchableHighlight
+          onPress={()=>{
+              nav.navigate("StackDetalleProducto");
+          }}
+          >
+            <View style={styles.fila}>
+              <View style={styles.imagen}>
+                <Avatar title={nombre.substring(0, 1)}> </Avatar>
+              </View>
+              <View style={styles.descripcion}>
+                <Text style={styles.textoDescripcion}> {nombre} </Text>
+              </View>
+              <View style={styles.precio}>
+                <Text style={styles.textoPrecio}> {precio} </Text>
+              </View>
+            </View>
+          </TouchableHighlight>
+          
         );
     }
 }
+
+
+const styles = StyleSheet.create({
+  fila: {
+    flex: 1,
+    flexDirection: "row",//eje pricipal es el horizontal
+    backgroundColor: "orange",
+    marginBottom:10,
+    // alignItems: "stretch",//aplica al eje transversal
+    // justifyContent: "center",//eje principal
+    paddingVertical:10,
+    // paddingTop: 10,
+    borderRadius: 25
+    
+  },
+
+  imagen:{
+    // backgroundColor: "blue",
+    flex:2,
+    alignItems: "center"
+  },
+  descripcion: {
+    flex: 5,
+    // backgroundColor: "gray",
+    justifyContent: "center",
+    
+  },
+  precio: {
+    flex: 2,
+    // backgroundColor: "red",
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+
+  textoDescripcion:{
+    fontSize:20,
+    fontWeight:"bold"
+  },
+  textoPrecio: {
+    fontSize: 15,
+    fontWeight: "bold",
+    paddingRight:10
+  },
+});
